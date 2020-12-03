3f9907d7e20ed97eea720715c778c5013aaf81c6
geon <0408gun@gmail.com>
2020-12-03 16:59:50 +0900

    [feat] 프로젝트에 JPA적용
    H2 데이터베이스로 테스트
    객체로 DB에 Create, Read함
    application.properties의 옵션 설정으로 쿼리를 로그로 확인 가능

---
+++ src/main/java/com/gunt/balck/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.gunt.balck.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
