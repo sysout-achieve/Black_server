730509373befe3227d1fcfefab9c974b232ef994
geon <0408gun@gmail.com>
2020-12-04 15:41:38 +0900

    [feat] JPA Auditing으로 생성/수정 시간 자동화
    BaseTimeEntity클래스는 모든 Entity의 상위클래스가 되어 createdDate, modifiedDate를 자동으로 관리하는 역할

--- src/main/java/com/gunt/balck/springboot/service/posts/PostsService.java
+++ src/main/java/com/gunt/balck/springboot/service/posts/PostsService.java
@@ -9,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.parser.Entity;
-
 @RequiredArgsConstructor
 @Service
 public class PostsService {
