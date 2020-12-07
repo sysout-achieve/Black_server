879812f539217cf76133c5fa71967f696a0d6bbb
geon <0408gun@gmail.com>
2020-12-08 00:02:25 +0900

    [feat] 구글 로그인 연동하기
    IndexController에서 세션정보 확인하여 index.mustache의 화면 ui 결정함
    권한 관리 추가하여 GUEST상태에서 글쓰기 불가, USER상태에서 가능

---
+++ src/main/java/com/gunt/balck/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.gunt.balck.springboot.config.auth;
+
+import com.gunt.balck.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()    //URL별 권한 관리 설정하는 옵션의 시작
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+    }
+}
