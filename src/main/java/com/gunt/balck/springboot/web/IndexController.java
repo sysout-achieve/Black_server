61510d69806ab390d250d2b53f05c378081fb856
geon <0408gun@gmail.com>
2020-12-08 14:33:49 +0900

    [refactor] 어노테이션 기반으로 세션 적용
    어노테이션 기반으로 세션값 가져오기
    세션 값이 필요하면 그 때마다 직접 세션에서 값을 가져와야함
    메소드 인자로 세션값을 바로 받을 수 있도록 변경

--- src/main/java/com/gunt/balck/springboot/web/IndexController.java
+++ src/main/java/com/gunt/balck/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.gunt.balck.springboot.web;
 
+import com.gunt.balck.springboot.config.auth.LoginUser;
 import com.gunt.balck.springboot.config.auth.dto.SessionUser;
 import com.gunt.balck.springboot.service.posts.PostsService;
 import com.gunt.balck.springboot.web.dto.PostResponseDto;
@@ -20,9 +21,8 @@ public class IndexController {
 
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null){
             model.addAttribute("userName", user.getName());
         }
