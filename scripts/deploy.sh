115f33e2c844409c3060e032555815e503c5bc3f
geon <0408gun@gmail.com>
2020-12-14 17:10:22 +0900

    [feat] 버전 업데이트 자동 배포 test
    자동 배포 테스트용 푸시

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ubuntu/app/step2
-PROJECT_NAME=was-service
+PROJECT_NAME=Black_server
 
 echo "> Build 파일 복사"
 
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl was-service | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl Black_server | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 PID : $CURRENT_PID"
 
